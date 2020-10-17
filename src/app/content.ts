6abde5d35b185f9d609364d09210fbfd921c7320
utakeru <takeru.u.cb@gmail.com>
2020-10-17 17:53:08 +0900

    initial commit

---
+++ src/app/content.ts
@@ -0,0 +1,4 @@
+import "../manifest.json"
+import "../styles/content.scss"
+
+console.log("hoge")
---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const path = require("path")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CopyPlugin = require("copy-webpack-plugin")
+const webpack = require("webpack")
+
+const environment = process.env.NODE_ENV || "development"
+const ASSET_PATH = process.env.ASSET_PATH || "/"
+
+module.exports = {
+  mode: environment,
+  devtool: "inline-source-map",
+
+  entry: {
+    content: "./src/app/content.ts",
+    background: "./src/app/background.ts",
+  },
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js",
+    publicPath: ASSET_PATH,
+  },
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+      chunkFilename: "css/[id].css",
+    }),
+    new CopyPlugin({ patterns: [{ from: "src/icons/", to: "icons" }] }),
+    new webpack.DefinePlugin({
+      "process.env.ASSET_PATH": JSON.stringify(ASSET_PATH),
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: environment === "development",
+            },
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === "development",
+            },
+          },
+          "css-loader",
+        ],
+      },
+      {
+        test: /\/manifest\.json$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+            },
+          },
+          "extract-loader",
+          {
+            loader: "chrome-manifest-loader",
+            options: {
+              mapVersion: true,
+            },
+          },
+        ],
+        type: "javascript/auto",
+      },
+    ],
+  },
+}
