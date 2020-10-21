8d4f857e5663f260e2fe1ed701e4e01eb5ef8cca
Ryota Konno <rkonno.rb@gmail.com>
2020-10-21 22:35:55 +0900

    ドロップダウンメニューを選ぶとソートする

--- src/app/content.ts
+++ src/app/content.ts
@@ -16,15 +16,27 @@ const SORTONE_COMMENTS_WRAPPER_CLASSNAME = "sortone-comments-wrapper"
 const domObserver = new DomObserver()
 domObserver.startCommentComponentObserver()
 
+let postEls: HTMLElement[]
+
 document.addEventListener(EventType.COMMENT_COMPONENT_LOADED, (e) => {
   renderDropdown()
 
   const targetEl = (e as CustomEvent).detail.element
   console.log("comment component loaded", targetEl)
-  const postEls = getPosts(targetEl)
+  postEls = getPosts(targetEl)
   console.log(postEls)
   hideOriginPosts(postEls)
-  const sortedPosts = sortPostElements(postEls, SortOrder.LIKE_DESC)
+  const wrapperEl = insertCommentsWrapperElement(
+    SORTONE_COMMENTS_WRAPPER_CLASSNAME
+  )
+  if (!wrapperEl) {
+    return
+  }
+  renderPosts(postEls, wrapperEl)
+})
+
+export const sortPost = (order: SortOrder) => {
+  const sortedPosts = sortPostElements(postEls, order)
   console.log(sortedPosts)
 
   const wrapperEl = insertCommentsWrapperElement(
@@ -34,4 +46,4 @@ document.addEventListener(EventType.COMMENT_COMPONENT_LOADED, (e) => {
     return
   }
   renderPosts(sortedPosts, wrapperEl)
-})
+}
--- src/app/sortone-ui/dropdown.ts
+++ src/app/sortone-ui/dropdown.ts
@@ -1,7 +1,14 @@
 import "../../styles/sortone-ui/dropdown.scss"
 import { html, render } from "lit-html"
+import { sortPost } from "../content"
+import { SortOrder } from "../kintone/space-thread"
 
-const menus = ["------", "いいねが多い順", "日付が新しい順", "返信が多い順"]
+const THREAD_BODY_CLASSNAME = "ocean-space-thread-body"
+const SORTONE_UI_DROPDOWN_PARNT_CLASSNAME = "sortone-ui-dropdown-parent"
+const SORTONE_UI_DROPDOWN_CLASSNAME = "sortone-ui-dropdown"
+const SORTONE_UI_SELECT_MENU_CLASSNAME = "sortone-ui-select-menu"
+
+const menus = ["------", "いいねが多い順", "いいねが少ない順"]
 
 let selected = 0
 
@@ -11,17 +18,25 @@ const onChangeMenu = {
       `onChangeMenu.Test: 「${menus[e.target.value]}」が選択されました`
     )
     selected = e.target.value
+    switch (menus[e.target.value]) {
+      case "いいねが多い順":
+        sortPost(SortOrder.LIKE_DESC)
+        break
+      case "いいねが多い順":
+        sortPost(SortOrder.LIKE_ASC)
+        break
+      default:
+        break
+    }
   },
 }
 
 const insertDropdownWrapper = () => {
-  const threadBody = document.getElementsByClassName(
-    "ocean-space-thread-body"
-  )[0]
-  threadBody.className += " sortone-ui-dropdown-parent"
+  const threadBody = document.getElementsByClassName(THREAD_BODY_CLASSNAME)[0]
+  threadBody.className += ` ${SORTONE_UI_DROPDOWN_PARNT_CLASSNAME}`
 
   const element = document.createElement("div")
-  element.className = "sortone-ui-dropdown"
+  element.className = SORTONE_UI_DROPDOWN_CLASSNAME
 
   threadBody.insertAdjacentElement("beforeend", element)
   return element
@@ -35,7 +50,7 @@ const insertDropdown = () => {
   })
 
   return html`
-    <div class="sortone-ui-select-menu">
+    <div class="${SORTONE_UI_SELECT_MENU_CLASSNAME}">
       <select @change=${onChangeMenu}>
         ${optionTemplates}
       </select>
