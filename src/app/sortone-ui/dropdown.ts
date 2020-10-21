10dd41632c1a8ecc14652e2cd49acadcb9dd8beb
Ryota Konno <rkonno.rb@gmail.com>
2020-10-21 15:42:49 +0900

    ドロップダウンメニューのテンプレートを作成

--- src/app/sortone-ui/dropdown.ts
+++ src/app/sortone-ui/dropdown.ts
@@ -1,6 +1,15 @@
 import "../../styles/sortone-ui/dropdown.scss"
 import { html, render } from "lit-html"
 
+const menus = [
+    "-----",
+    "いいねが多い順",
+    "日付が新しい順",
+    "返信が多い順",
+]
+
+let selected = "-----"
+
 const insertDropdownWrapper = () => {
     const threadBody = document.getElementsByClassName("ocean-space-thread-body")[0]
     threadBody.className += " sortone-ui-dropdown-parent"
@@ -13,39 +22,30 @@ const insertDropdownWrapper = () => {
 }
 
 const insertDropdown = () => {
+    const menusTemplates = [];
+    for (const m of menus) {
+        menusTemplates.push(
+            html`
+                <div class="goog-menuitem goog-option ${selected === m ? "goog-option-selected goog-menuitem-highlight" : ""}">
+                    <div class="goog-menuitem-content">
+                        <div class="goog-menuitem-checkbox"></div>
+                        ${m}
+                    </div>
+                </div>
+            `
+        );
+    }
+
     return html`
         <div class="select-cybozu">
             <div class="gaia-argoui-select">
-                <span class="gaia-argoui-select-label">-----</span>
+                <span class="gaia-argoui-select-label">${selected}</span>
                 <span class="gaia-argoui-select-arrow"></span>
             </div>
         </div>
         <div class="sortone-ui-selectmenu">
             <ul class="gaia-argoui-selectmenu gaia-argoui-selectmenu-vertical sortone-ui-selectmenu" style="visibility: visible; min-width: 198px">
-                <div class="goog-menuitem goog-option goog-option-selected goog-menuitem-highlight">
-                    <div class="goog-menuitem-content">
-                        <div class="goog-menuitem-checkbox"></div>
-                        -----
-                    </div>
-                </div>
-                <div class="goog-menuitem goog-option">
-                    <div class="goog-menuitem-content">
-                        <div class="goog-menuitem-checkbox"></div>
-                        いいね順
-                    </div>
-                </div>
-                <div class="goog-menuitem goog-option">
-                    <div class="goog-menuitem-content">
-                        <div class="goog-menuitem-checkbox"></div>
-                        日付が新しい順
-                    </div>
-                </div>
-                <div class="goog-menuitem goog-option">
-                    <div class="goog-menuitem-content">
-                        <div class="goog-menuitem-checkbox"></div>
-                        返信が多い順
-                    </div>
-                </div>
+                ${menusTemplates}
             </ul>
         </div>
     `
